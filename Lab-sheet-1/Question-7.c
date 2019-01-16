90a4431e8ad57979914738a0c0fb56cde554ea3f
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-01-16 20:12:19 +0530

    LS1: Question-7 V1

---
+++ Lab-sheet-1/Question-7.c
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#define ll long long
+
+char* reverse(char* t,int a,int b) {
+	char temp[100];
+	ll c = 0;
+	for (int i = a; i <= b; i++) {
+		temp[c] = t[i];
+		c++;
+	}
+	c = 0;
+	for (int i = b; i >= a; i--) {
+		t[i] = temp[c];
+		c++;
+	}
+	return t;
+}
+
+char* alternate(char* text) {
+	int i = 0;
+    ll c = 0;
+	while(text[i] != '\n') {
+    	if (text[i] != ' ') {
+    		if (c%2 == 0) {
+		    	int x = text[i] - 'a';
+		    	x = (x+3)%25;
+		    	text[i] = (char)(x+'a');
+	    }
+	    	c++;
+    	}
+    	i++;
+    }
+    return text;
+}
+
+char* decode_alternate(char* text) {
+	int i = 0;
+    ll c = 0;
+	while(text[i] != '\n') {
+    	if (text[i] != ' ') {
+    		if (c%2 == 0) {
+		    	int x = text[i] - 'a';
+		    	x = (x-3)%25;
+		    	text[i] = (char)((char)x+'a');
+	    }
+	    	c++;
+    	}
+    	i++;
+    }
+    return text;
+}
+
+char* encode(char* text) {
+
+	alternate(text);
+	int i = 0;
+	int a = 0, b = 0;
+	while(text[i] != '\n') {
+
+		if (text[i] == ' ') {
+			b = i-1;
+			reverse(text, a, b);
+			a = i+1;
+		}
+
+		i++;
+	}
+
+	reverse(text, a, i-1);
+	return text;
+}
+
+char* decode(char* text) {
+	int i = 0;
+	int a = 0, b = 0;
+	while(text[i] != '\n') {
+
+		if (text[i] == ' ') {
+			b = i-1;
+			reverse(text, a, b);
+			a = i+1;
+		}
+
+		i++;
+	}
+
+	reverse(text, a, i-1);
+	decode_alternate(text);
+	return text;
+}
+
+int main() {
+	    //Using text files for input output
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    char text[100];
+    fgets(text, 100, stdin);
+
+    
+    encode(text);
+    printf("%s", text);
+    decode(text);
+    printf("%s", text);
+    
+	return 0;
+}
