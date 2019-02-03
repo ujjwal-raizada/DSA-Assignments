fa31899f935bf036158517339e4d887d8bd991a6
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-02-04 01:10:56 +0530

    DSA: Add C template

---
+++ template.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define ll long long
+int main() {
+
+    //Using text files for input output
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    
+
+
+    return 0;
+}
